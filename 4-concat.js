0c65acd92b8bc76c7b2db4eb256e287c3d37b5f2
jac <jackipbore@gmail.com>
2025-11-09 23:27:30 +0300

    Add script that prints two arguments in '<arg1> is <arg2>' format

---
+++ 4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Script that prints "<first argument> is <second argument>"
+
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+console.log(`${firstArg} is ${secondArg}`);
