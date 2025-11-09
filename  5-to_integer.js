782a1df5b6270e22d9fb692cac4b092c9bf93f19
jac <jackipbore@gmail.com>
2025-11-09 23:32:18 +0300

    Add script that prints first argument as integer or 'Not a number'

---
+++  5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script that prints the first argument as integer or "Not a number"
+
+const arg = process.argv[2];         // Get the first argument
+const num = parseInt(arg);           // Convert it to an integer
+
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
+}
