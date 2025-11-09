d73ae610df2446732ceb948b7089851770df8093
jac <jackipbore@gmail.com>
2025-11-09 23:23:57 +0300

    Add script that prints first argument or 'No argument'

---
+++ 3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that prints the first argument passed to it
+
+const firstArg = process.argv[2]; // The first real argument is always index 2
+
+if (firstArg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(firstArg);
+}
