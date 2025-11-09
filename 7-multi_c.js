93eaa79011a15c2ecbef91a6a00bfd14210dd55a
jac <jackipbore@gmail.com>
2025-11-09 23:43:10 +0300

    Add script that prints 'C is fun' x times or error if invalid

---
+++ 7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Script that prints "C is fun" x times
+
+const arg = process.argv[2];
+const count = parseInt(arg);
+
+if (isNaN(count) || count <= 0) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+}
